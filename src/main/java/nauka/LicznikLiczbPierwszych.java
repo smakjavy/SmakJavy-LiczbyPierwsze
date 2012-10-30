7f0924a5a466602f0f75bdaf73a3fd8fa1b84464
Marcin Grabowiecki <MarcinGrabowiecki@simple.com.pl>
2012-10-30 15:35:58 +0100

    interfejs do zaimplementowania

---
+++ src/main/java/nauka/LicznikLiczbPierwszych.java
@@ -0,0 +1,17 @@
+package nauka;
+
+public interface LicznikLiczbPierwszych {
+	
+	/**
+	 * liczy liczby pierwsze
+	 * @param max maksymalna liczba, dla której odbedzie sie liczenie czy jest pierwsza
+	 * @return tablia liczb pierwszych
+	 */
+	
+	public int[] liczLiczbyPierwsze(int max);
+	/**
+	 * podaje czas liczenia
+	 * @return czas liczenia w milisekundach
+	 */
+	public long dajCzasLiczenia();
+}
