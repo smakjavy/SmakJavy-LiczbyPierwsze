e7fff73d41bc4ce9f87f13dfdfbb1c95bf919209
Monika Czaplińska <nika.c30@gmail.com>
2012-11-16 17:47:33 +0100

    Zabawy z JavaFX

--- src/main/java/raczkowanieFX/HelloFX.java
+++ src/main/java/raczkowanieFX/HelloFX.java
@@ -8,6 +8,11 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.image.ImageViewBuilder;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+
 
 public class HelloFX extends Application{
 	
@@ -21,26 +26,38 @@ public class HelloFX extends Application{
 		
 	}
 
-
 	@Override
-	public void start(Stage primaryStage) throws Exception {
+	public void start(final Stage primaryStage) throws Exception {
 		// TODO Auto-generated method stub
-		Group root = new Group();
-		String imageSource = "http://www.alemiodzio.pl/vardata/galeria/10000002/n_smieszne008.jpg"; 
+		
+            primaryStage.setTitle("Moja pogoñ za Jav¹!");
+            Button btn = new Button();
+            btn.setText("Nie mogê poznaæ smaku Javy! Naciœnij aby sprawdŸ dlaczego :)");
+            btn.setOnAction(new EventHandler<ActionEvent>() {
+                public void handle(ActionEvent event) {
+                	Group root = new Group();
+            		String imageSource = "http://www.alemiodzio.pl/vardata/galeria/10000002/n_smieszne008.jpg"; 
 
-        ImageView imageView = ImageViewBuilder.create()
-                .image(new Image(imageSource))
-                .build();
-         
-        Group myGroup = GroupBuilder.create()
-                .children(imageView)
-                .build();
-         
-        root.getChildren().add(myGroup);
+                    ImageView imageView = ImageViewBuilder.create()
+                            .image(new Image(imageSource))
+                            .build();
+                     
+                    Group myGroup = GroupBuilder.create()
+                            .children(imageView)
+                            .build();
+                     
+                    root.getChildren().add(myGroup);
 
-        primaryStage.setScene(new Scene(root, 500, 312));
-		primaryStage.setTitle("Moja pogoñ za Jav¹ :p");
-        primaryStage.show();
-			
-	}
+                    primaryStage.setScene(new Scene(root, 500, 312));
+            		primaryStage.setTitle("Moja pogoñ za Jav¹ :p");
+                    primaryStage.show();;
+                }
+            });
+            
+            StackPane root = new StackPane();
+            root.getChildren().add(btn);
+            primaryStage.setScene(new Scene(root, 500, 312));
+            primaryStage.show();              
+	}    
 }
+
